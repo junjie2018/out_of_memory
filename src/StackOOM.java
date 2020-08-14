27d29fa547be0fd53cc71c6f8f77580909b4f459
小桀 <812797569@qq.com>
2020-08-14 16:06:19 +0800

    auto commit

--- src/StackOOM.java
+++ src/StackOOM.java
@@ -1,11 +1,15 @@
 // 该实验无法再jdk8上运行
+// jdk7 不支持lambda
 public class StackOOM {
     public void stackLeakByThread() {
         while (true) {
             {
-                new Thread(() -> {
-                    while (true) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
 
+                        }
                     }
                 }).start();
             }
